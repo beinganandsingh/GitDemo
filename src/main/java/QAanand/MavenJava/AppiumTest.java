439ef89030a907916d8a627b8eae660b1c2056d9
beinganandsingh <anandsingh0028@gmail.com>
2021-08-30 22:59:14 +0530

    first commit

---
+++ src/main/java/QAanand/MavenJava/AppiumTest.java
@@ -0,0 +1,17 @@
+package QAanand.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	 @Test
+	 public void NativeAppAndroid()
+	 {
+		System.out.println("NativeAppAndroid");
+	 }
+	 @Test
+	 public void IOSapp()
+	 {
+		 System.out.println("IOSapp");
+	 }
+
+}
---
+++ src/main/java/QAanand/MavenJava/RestAPI.java
@@ -0,0 +1,18 @@
+package QAanand.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RestAPI {
+	 @Test
+	 public void postJira()
+	 {
+		 System.out.println("postJira");
+	 }
+	 @Test
+	 public void deleteTwitter()
+	 {
+		 System.out.println("deleteTwitter");
+	 }
+
+
+}
